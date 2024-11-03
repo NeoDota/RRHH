0f7e6c3fa7324dbcc25dbce7a1d138383de3aeda
Diego Lucana Sanchez <neodota007@gmail.com>
2024-11-02 21:06:07 -0400

    Se hizo las migraciones y relaciones | recien se hara resource de filament .V3

--- database/migrations/2024_11_03_000918_create_atencions_table.php
+++ database/migrations/2024_11_03_000918_create_atencions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('atencions', function (Blueprint $table) {
             $table->id();
 
+            $table->string('cite');
             $table->string('cargo');
             $table->string('lugar');
             $table->string('red');
